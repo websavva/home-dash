6c836a1d52079fd4f988a8835b97ab51d44d842c
WebSavva <websavvap@gmail.com>
2024-07-16 23:42:00 +0300

    feat: useBookmarkManager hook was added too

---
+++ src/hooks/use-bookmark-manager.ts
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+
+import { BookmarkManagerContext } from "@/context/bookmark-manager";
+
+export const useBookmarkManager = () => {
+  return useContext(BookmarkManagerContext);
+};
