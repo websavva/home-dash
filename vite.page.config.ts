22ca1de80e42c246a3557bb6e8e9f8f72f13013b
WebSavva <websavvap@gmail.com>
2024-07-29 22:15:13 +0300

    feat: Responsivenes of styles was ensured

--- vite.page.config.ts
+++ vite.page.config.ts
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react';
 
 import { baseConfig, resolvePath } from './vite.base.config';
 
+import breakpoints from './src/page/sass/partials/breakpoints.json';
+
 const sassPartialsSrc = resolvePath('src/page/sass/partials');
+
+const mediaQueryDeclarations = Object.entries(breakpoints)
+  .map(([name, size]) => [
+    [`$max-${name}: "(max-width: ${size}px)";`],
+    [`$min-${name}: "(min-width: ${size + 1}px)";`],
+  ])
+  .flat();
 
 export default mergeConfig(
   baseConfig,
@@ -19,9 +28,11 @@ export default mergeConfig(
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: ["@use 'sass:map';", "@use 'context' as *;"].join(
-            '\n',
-          ),
+          additionalData: [
+            "@use 'sass:map';",
+            "@use 'context' as *;",
+            ...mediaQueryDeclarations,
+          ].join('\n'),
           includePaths: [sassPartialsSrc],
         },
       },
